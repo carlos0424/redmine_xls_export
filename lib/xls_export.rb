127b21e6a397244bc2957055a36e53653ff5dc4e
Carlos <carlosarbelaez0424@hotmail.com>
2024-10-21 22:16:38 -0500

    Updated init.rb to fix dependency load issues and remove deprecated dispatcher reference

--- lib/xls_export.rb
+++ lib/xls_export.rb
@@ -45,6 +45,42 @@ module Redmine
   end
 end
 
+class XLS_QueryColumn
+  attr_accessor :name, :sortable, :groupable, :default_order
+  include Redmine::I18n
+
+  def initialize(name, options={})
+    self.name = name
+    self.sortable = options[:sortable]
+    self.groupable = options[:groupable] || false
+    if groupable == true
+      self.groupable = name.to_s
+    end
+    self.default_order = options[:default_order]
+    @caption_key = options[:caption] || "field_#{name}"
+  end
+
+  def caption
+    l(@caption_key)
+  end
+
+  def sortable?
+    !@sortable.nil?
+  end
+
+  def sortable
+    @sortable.is_a?(Proc) ? @sortable.call : @sortable
+  end
+
+  def value(issue)
+    issue.send name
+  end
+
+  def css_classes
+    name
+  end
+end
+
 # Asegúrate de que StripHTML está cargado aquí
 class XLS_JournalQueryColumn < XLS_QueryColumn
   include CustomFieldsHelper
